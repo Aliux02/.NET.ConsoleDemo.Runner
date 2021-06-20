9ff0ee020be7b76d920c60d23f476b52b1065534
Aliux <aliux02@gmail.com>
2021-06-20 22:34:21 +0300

    Update Program.cs

--- ConsoleDemo/Program.cs
+++ ConsoleDemo/Program.cs
@@ -7,20 +7,66 @@ namespace ConsoleDemo
     {
         static void Main(string[] args)
         {
+            var firstNumber = 0;
+            var secondtNumber = 0;
+            //var operator = 0;
+
+
             var input = Console.ReadLine();
 
-            int number;
+            var values = input.Split(' ');
 
-            var succeded = Int32.TryParse(input, out number);
+            List<string> numbers = new List<string>();
 
-            if(succeded == true)
+            foreach (var value in values)
             {
-                Console.WriteLine($"Input was multiplied by 2 {number*2}");
+                if ( value != "")
+                {
+                    numbers.Add(value);
+                }
             }
-            else
+
+            if (numbers[0] !="" && numbers[2] !="")
             {
-                Console.WriteLine("Error while parsing");
+                firstNumber = Int32.Parse(numbers[0]);
+                secondtNumber = Int32.Parse(numbers[2]);
             }
+
+            switch (numbers[1])
+            {
+                case "-":
+                    Console.WriteLine(firstNumber - secondtNumber);
+                    break;
+                case "+":
+                    Console.WriteLine(firstNumber + secondtNumber);
+                    break;
+                case "*":
+                    Console.WriteLine(firstNumber * secondtNumber);
+                    break;
+                case "/":
+                    Console.WriteLine(firstNumber / secondtNumber);
+                    break;
+                default:
+                    Console.WriteLine("Something went wrong");
+                    break;
+                    
+        }
+
+
+
+            //int number;
+            //Console.WriteLine(words);
+
+            //var succeded = Int32.TryParse(input, out number);
+
+            //if(succeded == true)
+            //{
+            //    Console.WriteLine($"Input was multiplied by 2 {number*2}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Error while parsing");
+            //}
         }
     }
 }
