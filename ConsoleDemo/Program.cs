d4326041f9f44fce4c979db128949d5671c054b4
Aliux <aliux02@gmail.com>
2021-06-20 15:13:33 +0300

    Add project files.

---
+++ ConsoleDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            int number;
+
+            var succeded = Int32.TryParse(input, out number);
+
+            if(succeded == true)
+            {
+                Console.WriteLine($"Input was multiplied by 2 {number*2}");
+            }
+            else
+            {
+                Console.WriteLine("Error while parsing");
+            }
+        }
+    }
+}
